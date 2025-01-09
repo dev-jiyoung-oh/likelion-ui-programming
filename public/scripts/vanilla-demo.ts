49c0c332a613b511cf0b7ebfe1e64269951150c1
jiyoung <aa991102@naver.com>
2025-01-09 13:35:55 +0900

    [정리] 코드 구성

--- public/scripts/vanilla-demo.ts
+++ public/scripts/vanilla-demo.ts
@@ -1,4 +1,8 @@
-import { Greeting } from "./greeting";
+/* -------------------------------------------------------------------------- */
+/* Vanilla Programming                                                        */
+/* -------------------------------------------------------------------------- */
+
+import { Greeting } from './greeting';
 
 // 인사말 리스트(목록) 배열
 const greetingList = Object.values(Greeting); // values
@@ -9,17 +13,17 @@ const greetingEntryList = Object.entries(Greeting); // [[key, value], ...]
 
 // 제어 대상 생성 o / 참조 x
 // const heading = document.querySelector('h1');
-const heading = document.createElement("h1");
+const heading = document.createElement('h1');
 heading.textContent = greetingList.at(0) as string;
 
 // 제어 버튼 생성 o / 참조 x
 // const changeButton = document.querySelector('[type="button"]');
-const changeButton = document.createElement("button");
-changeButton.setAttribute("type", "button");
-changeButton.textContent = "인사말";
+const changeButton = document.createElement('button');
+changeButton.setAttribute('type', 'button');
+changeButton.textContent = '인사말';
 
 // 버튼에 이벤트 바인딩
-changeButton?.addEventListener("click", handleChangeGreetMessage);
+changeButton?.addEventListener('click', handleChangeGreetMessage);
 
 // 이벤트 핸들러
 function handleChangeGreetMessage() {
@@ -30,7 +34,7 @@ function handleChangeGreetMessage() {
 }
 
 // 마운트(mount)할 DOM 엘리먼트 노드를 참조
-const app = document.getElementById("app");
+const app = document.getElementById('vanilla');
 
 // 동적으로 생성된 엘리먼트 노드를 참조한 DOM 노드에 마운트
 app?.prepend(heading, changeButton);
