a344a8864e56c2fdfd4958a53fafb6b664ac433a
jiyoung <aa991102@naver.com>
2025-01-09 15:21:08 +0900

    [실습] React.Fragment (조각, 파편) 빌트인 컴포넌트
    - 실제 DOM 렌더링 안함
    - <template> 요소와 유사

--- public/scripts/react-demo.ts
+++ public/scripts/react-demo.ts
@@ -4,34 +4,33 @@
 import React from '../lib/react.js';
 import ReactDOM from '../lib/react-dom/client.js';
 
-// React version
-// console.log('React version =', React.version);
-
-// ReactDOM/client version
-// console.log('ReactDOM version =', ReactDOM.version);
-
-/* -------------------------------------------------------------------------- */
-/* Creating React Element Node                                                */
-/* -------------------------------------------------------------------------- */
-
-// React API
-// React.createElement(type, props, ...children)
-
-const heading = React.createElement(
-  'h1',
-  {},
-  '안녕!' // children
+let heading = React.createElement(
+  'h1', 
+  {}, 
+  '안녕!'
 );
 
 const changeButton = React.createElement(
   'button',
-  {
-    type: 'button',
-  },
-  '인사말' // children
+  { type: 'button' },
+  '인사말'
 );
 
-console.group('React 요소 노드');
-console.dir(heading);
-console.dir(changeButton);
-console.groupEnd();
+// Namespace.module
+// React.Fragment (function)
+
+// 의미 없는 <div> 대신에 <React.Fragment>를 사용
+// 의미 없는 구조는 실제 DOM에 렌더링 되지 않아요!!!
+// console.log(React.Fragment);
+
+const wrapper = React.createElement(
+  React.Fragment, // type: HTMLTagNameString(e.g: 'a', 'table', ...) / Buit-in Component
+  null,
+  heading,
+  changeButton
+);
+
+const rootElement = document.getElementById('react');
+const reactDomRoot = ReactDOM.createRoot(rootElement);
+
+reactDomRoot.render(wrapper);
