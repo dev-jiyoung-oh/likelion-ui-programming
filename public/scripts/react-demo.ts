e033e6890d060f60a4c8a9ddcd51e95b1e64d77e
jiyoung <aa991102@naver.com>
2025-01-09 14:10:23 +0900

    [구성] 직접 만든 유틸리티 함수 분리

--- public/scripts/react-demo.ts
+++ public/scripts/react-demo.ts
@@ -1,8 +1,8 @@
 /* -------------------------------------------------------------------------- */
 /* React Programming Preview                                                  */
 /* -------------------------------------------------------------------------- */
-import React from "../lib/react.js";
-import ReactDOM from "../lib/react-dom/client.js";
+import React from '../lib/react.js';
+import ReactDOM from '../lib/react-dom/client.js';
 
 // React version
 // console.log('React version =', React.version);
@@ -18,48 +18,20 @@ import ReactDOM from "../lib/react-dom/client.js";
 // React.createElement(type, props, ...children)
 
 const heading = React.createElement(
-  "h1",
+  'h1',
   {},
-  "안녕!" // children
+  '안녕!' // children
 );
 
 const changeButton = React.createElement(
-  "button",
+  'button',
   {
-    type: "button",
+    type: 'button',
   },
-  "인사말" // children
+  '인사말' // children
 );
 
-console.group("React 요소 노드");
+console.group('React 요소 노드');
 console.dir(heading);
 console.dir(changeButton);
-console.groupEnd();
-
-/* -------------------------------------------------------------------------- */
-/* Own CreateElement Function                                                 */
-/* -------------------------------------------------------------------------- */
-
-function createElement(type: string, props: Record<string, any> = {}, ...children: any[]) {
-  return {
-    $$typeof: Symbol("own.element"),
-    key: null,
-    type,
-    props: {
-      ...props,
-      children,
-    },
-  };
-}
-
-const ownElement = createElement(
-  "div",
-  {
-    id: "own",
-  },
-  "hello"
-);
-
-console.group("Own 요소 노드");
-console.log(ownElement);
 console.groupEnd();
