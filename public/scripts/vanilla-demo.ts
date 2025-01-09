42293bddc6da624b155889f5e15ea6527cc59fb4
jiyoung <aa991102@naver.com>
2025-01-09 14:01:51 +0900

    [실습] React 엘리먼트(요소)
    - React 요소란?
    - 직접 만드는 추상 요소

--- public/scripts/vanilla-demo.ts
+++ public/scripts/vanilla-demo.ts
@@ -2,7 +2,7 @@
 /* Vanilla Programming                                                        */
 /* -------------------------------------------------------------------------- */
 
-import { Greeting } from './greeting';
+import { Greeting } from "./greeting";
 
 // 인사말 리스트(목록) 배열
 const greetingList = Object.values(Greeting); // values
@@ -11,19 +11,28 @@ const greetingEntryList = Object.entries(Greeting); // [[key, value], ...]
 
 // JavaScript Markup and Event Binding & Mount DOM Element Node
 
+/* -------------------------------------------------------------------------- */
+/* Creating DOM Element Node                                                  */
+/* -------------------------------------------------------------------------- */
+
 // 제어 대상 생성 o / 참조 x
 // const heading = document.querySelector('h1');
-const heading = document.createElement('h1');
+const heading = document.createElement("h1");
 heading.textContent = greetingList.at(0) as string;
 
 // 제어 버튼 생성 o / 참조 x
 // const changeButton = document.querySelector('[type="button"]');
-const changeButton = document.createElement('button');
-changeButton.setAttribute('type', 'button');
-changeButton.textContent = '인사말';
+const changeButton = document.createElement("button");
+changeButton.setAttribute("type", "button");
+changeButton.textContent = "인사말";
+
+console.group("DOM 요소 노드");
+console.dir(heading);
+console.dir(changeButton);
+console.groupEnd();
 
 // 버튼에 이벤트 바인딩
-changeButton?.addEventListener('click', handleChangeGreetMessage);
+changeButton?.addEventListener("click", handleChangeGreetMessage);
 
 // 이벤트 핸들러
 function handleChangeGreetMessage() {
@@ -34,7 +43,7 @@ function handleChangeGreetMessage() {
 }
 
 // 마운트(mount)할 DOM 엘리먼트 노드를 참조
-const app = document.getElementById('vanilla');
+const app = document.getElementById("vanilla");
 
 // 동적으로 생성된 엘리먼트 노드를 참조한 DOM 노드에 마운트
 app?.prepend(heading, changeButton);
