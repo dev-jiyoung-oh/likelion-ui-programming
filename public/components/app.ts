917f98f3f9ff770bf8f3e099653de9531465f4de
jiyoung <aa991102@naver.com>
2025-01-10 15:00:10 +0900

    [실습] props.children

--- public/components/app.ts
+++ public/components/app.ts
@@ -1,6 +1,21 @@
 import React from '../lib/react.js';
 import Calculators from './calculators';
+import Box from './box';
 
+function App() {
+  return React.createElement(
+    'div',
+    {
+      className: 'app',
+    },
+    // createElement(type, props, ...children)
+    React.createElement(Box, null, '기본 박스'),
+    React.createElement(Box, null, '작은 박스'),
+    React.createElement(Box, null, '큰 박스')
+  );
+}
+
+/*
 const { createElement: h } = React;
 
 function App() {
@@ -12,5 +27,6 @@ function App() {
     h(Calculators)
   );
 }
+*/
 
 export default App;
